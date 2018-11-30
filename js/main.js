9dd48f4a234505a83c2f700d1941ee53db4d16ec
Thai Hung Nguyen <nthung@apcs.vn>
2018-11-30 10:39:11 +0700

    User profile

--- js/main.js
+++ js/main.js
@@ -25,3 +25,20 @@ function runScript(e) {
   }
 } 
 
+function changepic_click(){
+  var preview = document.querySelector('img'); //selects the query named img
+  var file    = document.querySelector('input[type=file]').files[0]; //sames as here
+  var reader  = new FileReader();
+
+  reader.onloadend = function () {
+      preview.src = reader.result;
+  }
+
+  if (file) {
+      reader.readAsDataURL(file); //reads the data as a URL
+  } else {
+      preview.src = "https://ptetutorials.com/images/user-profile.png";
+  }
+}
+
+
